9700a5af1ff9e9cbe98acdd98aca043718fc07bf
timothyyipp <145803651+timothyyipp@users.noreply.github.com>
2025-05-24 12:12:32 -0400

    attempted

--- auth.js
+++ auth.js
@@ -1,7 +1,1 @@
-const auth=() => {
-  return encodeURI(`https://linkedin.com/oauth/v2/authorization?client_id=${process.env.LINKEDIN_CLIENT_ID}&response_type=code&scope=${process.env.SCOPE}&redirect_uri=${process.env.REDIRECT_URI}`); 
-}
 
-module.exports = {
-  auth
-};
